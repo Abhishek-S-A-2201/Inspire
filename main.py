88a1c3e2196d2ce68ce34ee21ccbff77ad7dd7b0
Abhishek S A <88477888+Abhishek-S-A-2201@users.noreply.github.com>
2024-03-31 15:31:13 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,41 @@ import time
 import sys
 
 
+def download_models():
+
+    def download_hugginface(url, destination_folder, original_folder):
+        print(f"Downloading from {url}...")
+        os.chdir(destination_folder)
+        os.system(f"wget --content-disposition {url}")
+        os.chdir(original_folder)
+
+    download_hugginface(
+        "https://huggingface.co/Abhishek-Anand/Inspire/resolve/main/epicrealism_naturalSinRC1VAE.safetensors?download=true",
+        "./ComfyUI/models/checkpoints/",
+        "../../../"
+    )
+
+    download_hugginface(
+        "https://huggingface.co/Abhishek-Anand/Inspire/resolve/main/controlnet11Models_softedge.safetensors?download=true",
+        "./ComfyUI/models/controlnet/",
+        "../../../"
+    )
+
+    download_hugginface(
+        "https://huggingface.co/Abhishek-Anand/Inspire/resolve/main/model.safetensors?download=true",
+        "./ComfyUI/models/clip_vision/",
+        "../../../"
+    )
+    download_hugginface(
+        "https://huggingface.co/Abhishek-Anand/Inspire/resolve/main/ip-adapter-plus_sd15.safetensors?download=true",
+        "./ComfyUI/custom_nodes/ComfyUI_IPAdapter_plus/models/",
+        "../../../../"
+    )
+
+
+download_models()
+
+
 def start_comfyui():
     subprocess.run(["python", "./ComfyUI/main.py"])
 
